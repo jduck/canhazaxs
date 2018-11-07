0d42218cc568d3e3cfba0a0634c5b2ebeb5b5b7e
Joshua J. Drake <github.jdrake@qoop.org>
2018-11-07 14:15:28 -0600

    Support building static and pie binaries

--- Android.mk
+++ Android.mk
@@ -1,15 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_CFLAGS += -fPIE
-LOCAL_LDFLAGS += -fPIE -pie
-
-LOCAL_SRC_FILES := \
-  canhazaxs.c
-
-LOCAL_MODULE := charm
-
+LOCAL_SRC_FILES := canhazaxs.c
+LOCAL_MODULE := charm-static
 include $(BUILD_STATIC_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := canhazaxs.c
+LOCAL_MODULE := charm
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := canhazaxs.c
+LOCAL_MODULE := charm-pie
+LOCAL_CFLAGS += -Wall -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
+include $(BUILD_EXECUTABLE)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
